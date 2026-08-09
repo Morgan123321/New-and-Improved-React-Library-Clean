b4c183d1aa7bda8b675e3646ce6d164b9432e0b0
Morgan123321 <morgan.feeney1022@gmail.com>
2026-08-09 09:55:01 -0400

    loading

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -12,50 +12,61 @@ const [img,setImg]=useState();
 
 const mountedRef=useRef(true);
 
-useEffect (()=> {
-const image = new Image();
-image.src=book.url;
-image.onload = ()=> {
-   setTimeout(()=>{
-   if (mountedRef.current) {
-   setImg(image);
-   }
-}, 300);
-};
 
-return() => {
-   mountedRef.current=false;
-}
-})
+useEffect(() => {
+  mountedRef.current = true;
+
+  const image = new Image();
+  image.src = book.url;
+
+  image.onload = () => {
+    setTimeout(() => {
+      if (mountedRef.current) {
+        setImg(image);
+      }
+    }, 300);
+  };
+
+  return () => {
+    mountedRef.current = false;
+  };
+}, [book.url]);
 
 return (
-<div className="book">
-   {img ? (
-  // Book content
+{img ? (
+  <>
+    <Link to={`/books/${book.id}`}>
+      <figure className="book__img--wrapper">
+        <img
+          src={img.src}
+          alt={book.title}
+          className="book__img"
+        />
+      </figure>
+    </Link>
+
+    <div className="book__title">
+      <Link to={`/books/${book.id}`} className="book__title--link">
+        {book.title}
+      </Link>
+    </div>
+
+    <Rating rating={book.rating} />
+    <Price
+      salePrice={book.salePrice}
+      originalPrice={book.originalPrice}
+    />
+  </>
 ) : (
-  <div className="book__img--skeleton"></div>
+  <>
+    <div className="book__img--skeleton"></div>
+    <div className="skeleton book__title--skeleton"></div>
+    <div className="skeleton book__rating--skeleton"></div>
+    <div className="skeleton book__price--skeleton"></div>
+  </>
 )}
    
-   <Link to={`/books/${book.id}`}>
-                        <figure className="book__img--wrapper">
-                             <img src={img.src} alt={book.title} onLoad={imageLoaded} className="book__img--wrapper"/>
-  </figure>
-</Link>
-                               
-                            <div className="book__title">
-                            <Link to={`/books/${book.id}`} className="book__title--link">
-                             {book.title}
-                            </Link>
-                            </div>
-                            <Rating rating={book.rating}/>
-                           <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/>
-                     </>
-    ):(
-      <> <div className="book__img--skeleton"> </div>
-   <div className="skeleton book__title--skeleton"></div>
-   <div className="skeleton book__rating--skeleton"></div>
-   <div className="skeleton book__price--skeleton"></div></>
-    )}
+ 
   
    </div>
 );                
